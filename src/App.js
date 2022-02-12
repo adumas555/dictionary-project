91476ad37017b657c7acca860ee88d059a2530a5
adumas555 <91496550+adumas555@users.noreply.github.com>
2022-02-12 03:54:49 -0500

    Got rid of unnecessary component and implemented search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center App-footer">
+          <small>Coded by Angela Dumas</small>
+        </footer>
+      </div>
     </div>
   );
 }
